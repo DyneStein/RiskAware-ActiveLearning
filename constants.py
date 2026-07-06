867a9567449bc8553db8f6d84cba010d8235881c
Haider Ramzan <i240647@isb.nu.edu.pk>
2026-07-06 16:49:37 +0500

    Add risk mapping constants for skin conditions

---
+++ constants.py
@@ -0,0 +1,11 @@
+
+
+risk_mapping = {
+    'mel': 'high_risk',    # Melanoma
+    'bcc': 'high_risk',    # Basal cell carcinoma
+    'akiec': 'high_risk',  # Actinic keratoses
+    'nv': 'low_risk',      # Nevi (moles)
+    'bkl': 'low_risk',     # Benign keratosis
+    'df': 'low_risk',      # Dermatofibroma
+    'vasc': 'low_risk'     # Vascular lesions
+}
